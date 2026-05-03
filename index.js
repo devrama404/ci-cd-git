5d9ccced3be1838efe1bd0c2b43ec652de8d025c
devrama404 <devrama404@gmail.com>
2026-05-03 18:35:06 +0700

    update baru

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
- res.send("Hello dari CI/CD 🚀");
+ res.send("Hello dari CI/CD 123 🚀");
 });
 
 app.listen(3000, () => {
